6645818d56b5bdf345ca21600805da9497659505
LAETITIA-hub <laetitia.kamangu@student.moringaschool.com>
2025-04-24 10:51:28 +0300

    Update CabList.js

--- pick-and-go/src/components/CabList.js
+++ pick-and-go/src/components/CabList.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom"; 
 import CabStatus from "./CabStatus";
-import MapView from "./MapView";  // Import MapView
+import MapView from "./MapView";  
 
 function CabList() {
   const [cabs, setCabs] = useState([]);
